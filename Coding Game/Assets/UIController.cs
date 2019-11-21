eb9d11f6efa4c95bf53ab0689fcb8dc6cfd3022a
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-11-22 01:11:25 +0900

    [CREATE] 입력된 명령어 표시

---
+++ Coding Game/Assets/UIController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+  private List<GameObject> inputList;
+  public GameObject[] references;
+  private void Awake()
+  {
+    inputList = new List<GameObject>();
+  }
+
+  public int AddCommand(GetInput.INPUTKEY command)
+  {
+    Debug.Log(references.Length);
+    GameObject image = Instantiate(references[(int) command], transform);
+    
+    image.GetComponent<RectTransform>().localPosition = new Vector3(25 + 40 * inputList.Count, 25);
+    
+    inputList.Add(image);
+    
+    return inputList.Count;
+  }
+
+  public int RemoveCommand()
+  {
+    Destroy(inputList[inputList.Count - 1]);
+    inputList.RemoveAt(inputList.Count - 1);
+    return inputList.Count;
+  }
+}
